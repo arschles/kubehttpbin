a0170d3521d56c28c09741fd17cb16d1763a9ecc
Aaron Schlesinger <arschles@gmail.com>
2017-03-12 13:05:58 -0700

    deploying with helm in the deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,6 +5,11 @@
 
 cd "$(dirname "$0")" || exit 1
 
+curl -o helm https://storage.googleapis.com/kubernetes-helm/helm-v2.2.2-darwin-amd64.tar.gz
+chmod +x helm
+
 export IMAGE_PREFIX=arschles
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 make -C .. docker-build docker-push
+LAST_RELEASE=$(helm list -q)
+helm upgrade $LAST_RELEASE ../chart
