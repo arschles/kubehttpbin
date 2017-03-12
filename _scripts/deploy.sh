145f8156e72f961e43f29588d48cecf8e5709307
Aaron Schlesinger <arschles@gmail.com>
2017-03-12 13:23:00 -0700

    add the helm upgrade command
    along with all of the helm setup logic

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,11 +5,17 @@
 
 cd "$(dirname "$0")" || exit 1
 
-curl -o helm https://storage.googleapis.com/kubernetes-helm/helm-v2.2.2-darwin-amd64.tar.gz
-chmod +x helm
-
+# push to quay
 export IMAGE_PREFIX=arschles
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 make -C .. docker-build docker-push
+
+# tell helm where to find the kubeconfig file. note that travis will decrypt the kubeconfig
+# file to this location. See the 'before_install' step in the .travis.yml file
+export KUBECONFIG=./kubeconfig
+
+# download the helm CLI and calling 'helm upgrade'
+curl -o helm https://storage.googleapis.com/kubernetes-helm/helm-v2.2.2-darwin-amd64.tar.gz
+chmod +x helm
 LAST_RELEASE=$(helm list -q)
 helm upgrade $LAST_RELEASE ../chart
