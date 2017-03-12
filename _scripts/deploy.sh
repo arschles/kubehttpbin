ec4e6dac473a3f826200038f72068f035fb455cf
Aaron Schlesinger <arschles@gmail.com>
2017-03-12 13:29:29 -0700

    removing the encrypted kubeconfig and using kubeconfig env var

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -10,8 +10,9 @@ export IMAGE_PREFIX=arschles
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 make -C .. docker-build docker-push
 
-# tell helm where to find the kubeconfig file. note that travis will decrypt the kubeconfig
-# file to this location. See the 'before_install' step in the .travis.yml file
+# install the kubeconfig file from the env var, then tell helm where to find it 
+# (with the KUBECONFIG env var)
+echo $KUBECONFIG_DATA_BASE64 | base64 --decode > ./kubeconfig
 export KUBECONFIG=./kubeconfig
 
 # download the helm CLI and calling 'helm upgrade'
